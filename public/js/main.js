7c75f55c53d06fd275aa857f15849887d6c05dff
GlebKalinichenko <gleb.kalinichenko@umbrella-web.com>
2017-11-26 15:51:35 +0300

    Implement description for base features

--- public/js/main.js
+++ public/js/main.js
@@ -65,6 +65,10 @@
         handleMove(e); // draw a single point
     }
 
+    /**
+     * Handle move of cursor and send through socket io
+     * @param {Event} Event
+     * */
     function handleMove(e) {
         var newPos = eventToXY(e);
         var data = {start: oldPos, end: newPos, size: size, color: color};
@@ -77,12 +81,22 @@
         oldPos = {};
     }
 
+    /**
+     * Callback for start moving by press down gesture
+     * @param {String} Gesture type
+     * @param {Function} Callback handler
+     * */
     canvas.addEventListener('mousedown', function(e) {
         if (e.which == 1) {
             handleStart(e);
         }
     });
 
+    /**
+     * Callback for start moving
+     * @param {String} Gesture type
+     * @param {Function} Callback handler
+     * */
     canvas.addEventListener('mousemove', function(e) {
         if (e.buttons & 1) {
             handleMove(e);
@@ -91,6 +105,11 @@
 
     canvas.addEventListener('mouseout', handleEnd);
 
+    /**
+     * Callback for start moving by touch down gesture
+     * @param {String} Gesture type
+     * @param {Function} Callback handler
+     * */
     canvas.addEventListener('touchstart', function(e) {
         var touches = e.touches;
         if (touches.length == 1) {
@@ -98,6 +117,11 @@
         }
     });
 
+    /**
+     * Callback for start moving by touch move gesture
+     * @param {String} Gesture type
+     * @param {Function} Callback handler
+     * */
     canvas.addEventListener('touchmove', function(e) {
         var touches = e.touches;
         if (touches.length == 1) {
@@ -108,7 +132,11 @@
 
     canvas.addEventListener('touchend', handleEnd);
 
-    // remote events
+    /*
+    * Fetch lines by socket io from NodeJS server
+    * @params {String} Premitives
+    * @param {Function} Callback handler
+    * */
     socket.on('line', function(data) {
         line(data.start, data.end, data.size, data.color);
     });
